e1cd3440e27db4080583486535a9ad29e4339f13
ramyasurati <rsurati2000@gmail.com>
2019-11-13 15:07:11 +0530

    adding files

---
+++ WebApplication2/Models/Camp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Camp
+    {
+        [Key]
+           public int Cid { get; set; }
+            public string Ctopic { get; set; }
+            public int Cduration { get; set; }
+            public int Srequired { get; set; }
+
+            public string Fileuploaded { get; set; }
+
+    }
+}
